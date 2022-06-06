fcc5431153efe851213c3d8b379f723421cfdd49
Samuel Osei Adu <asamuel355@yahoo.com>
2022-06-06 12:14:09 +0000

    updates

--- admin/orders.php
+++ admin/orders.php
@@ -116,8 +116,8 @@
                                                                     <div class="dropdown dropdown-action">
                                                                         <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
                                                                         <div class="dropdown-menu dropdown-menu-right">
-                                                                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#edit_order"><i class="fa fa-pencil m-r-5"></i> Edit</a>
-                                                                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#view_order"><i class="fa fa-pencil m-r-5"></i> View Order</a>
+                                                                            <a class="dropdown-item" href="order-details.php?ord_id='.$reference_code.'"><i class="fa fa-pencil m-r-5"></i> View</a>
+                                                                            <a class="dropdown-item edit-order-main" edit-id= "'.$reference_code.'" href="#" data-toggle="modal"><i class="fa fa-pencil m-r-5"></i> Edit</a>
                                                                             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_order"><i class="fa fa-trash-o m-r-5"></i> Delete</a>
                                                                         </div>
                                                                     </div>
@@ -205,7 +205,7 @@
                 <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title">Edit Client</h5>
+                            <h5 class="modal-title">Change Status</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true">&times;</span>
 							</button>
@@ -213,53 +213,20 @@
                         <div class="modal-body">
                             <form>
                                 <div class="row">
-                                    <div class="col-12 text-center">
-                                        <div class="profile-img-wrap edit-img">
-                                            <img class="inline-block" src="assets/img/profiles/avatar-02.jpg" alt="user">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label class="col-form-label">Change Image <span class="text-danger">*</span></label>
-                                            <input class="form-control" type="file">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label class="col-form-label">Title</label>
-                                            <input class="form-control" type="text">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label class="col-form-label">Price <span class="text-danger">*</span></label>
-                                            <input class="form-control" type="number">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label class="col-form-label">Category <span class="text-danger">*</span> </label>
-                                            <select class="select form-control">
-                                                <option>Men Wear</option>
-                                                <option>Women Wear</option>
-                                                <option>Women Bag</option>
-                                                <option>Men Bag</option>
-                                                <option>Men Bag</option>
-                                                <option>Sneakers</option>
-                                            </select>
-                                        </div>
-                                    </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label class="col-form-label">Description <span class="text-danger">*</span></label>
-                                            <textarea class="form-control" name="description" id="description" cols="30" rows="4"></textarea>
+                                            <label class="col-form-label">Status <span class="text-danger">*</span> </label>
+                                            <select id="order_status" name="order-status" class="select form-control">
+                                                <option value="Pending">Pending</option>
+                                                <option value="Processing">Processing</option>
+                                                <option value="Ready for Delivery">Ready For Delivery</option>
+                                                <option value="Delivered">Delivered</option>
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="submit-section">
-                                    <button class="btn btn-primary submit-btn">Add</button>
+                                    <button class="btn btn-primary submit-btn update-order-status">Update Status</button>
                                 </div>
                             </form>
                         </div>
@@ -269,7 +236,7 @@
             <!-- /Edit Order Modal -->
 
             <!-- Delete Order Modal -->
-            <div class="modal custom-modal fade" id="delete_client" role="dialog">
+            <div class="modal custom-modal fade" id="delete_order" role="dialog">
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-body">
@@ -322,6 +289,46 @@
 
     <!-- Custom JS -->
     <script src="assets/js/app.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            $('.edit-order-main').on('click', function(e){
+                var edit_id = $(this).attr('edit-id');
+                $('#edit_order').modal('show');
+
+                $('.update-order-status').attr('edit_id', edit_id);
+            }); 
+
+            $('.update-order-status').on('click', function(e){
+                e.preventDefault();
+
+                var update_ref = $(this).attr('edit_id');
+                var order_status = document.getElementById('order_status').value;
+                console.log(update_ref);
+                console.log(order_status);
+                
+                $.ajax({
+                    url: 'php/update-order-status.php',
+                    method: 'GET',
+                    data: {update_ref: update_ref, order_status: order_status},
+
+                    success: function(data){
+                        if(data === 'success'){
+                            alert('Order Status Updated successfully');
+                            window.location.reload(true);
+                        }else{
+                            alert (data);
+                        }
+                    },
+
+                    error: function(err){
+                        console.log(err);
+                    }
+                })
+                
+            })
+        })
+    </script>
 
 </body>
 
---
+++ admin/php/update-order-status.php
@@ -0,0 +1,21 @@
+<?php
+    include_once "php/config.php";
+    $output = '';
+    if(isset($_GET['update_ref'])){
+        $update_ref = $_GET['update_ref'];
+        $order_status = $_GET['order_status'];
+        
+        $update = "UPDATE orders_info a, orders b, order_products c  SET a.status = '{$order_status}', b.status = '{$order_status}', c.status = '{$order_status}' WHERE a.reference_code = '{$update_ref}' ";
+        $qr_update = mysqli_query($con, $update);
+        if($qr_update){
+            echo "success";
+        }else{
+            echo "Sorry something went wrong updating the status";
+        }
+
+    }else{
+        header('location: orders.php');
+    }
+
+    echo $output;
+?>
